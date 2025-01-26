79d459da4a762a7943c00c443532836c6774386f
RezXHilda <rescategory@gmail.com>
2025-01-27 01:12:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-global.owner = ['62895331520602']  
-global.mods = ['62895331520602'] 
-global.prems = ['62895331520602']
-global.nameowner = 'Tio'
-global.numberowner = '62895331520602' 
-global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
+global.owner = ['6281802626072']  
+global.mods = ['6281802626072'] 
+global.prems = ['6281802626072']
+global.nameowner = 'Rez'
+global.numberowner = '6281802626072' 
+global.mail = '-' 
+global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmuyi'
+global.instagram = 'https://instagram.com/res030408'
+global.wm = '© Rez'
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
@@ -22,7 +22,7 @@ global.gcspam = false // Set true/false untuk menutup grup ketika spam (default:
 
 
 //INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = 'Iv7DelEg' 
 //Daftar terlebih dahulu https://api.botcahx.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
